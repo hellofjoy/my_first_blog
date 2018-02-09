30344fdc4ad92aecb8266fa1326ce1f823932fca
yhoh <yhoh@anapass.com>
2018-02-09 20:25:17 +0900

    Django admin
    1. change language : 'en-us' to 'ko'
    2. register model : admin.site.register(Post)
    3. super user 등록 : 'python manage.py createsuperuser'

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,15 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    def __init__(self):
-        self.author = models.ForeignKey('auth.User')
-        self.title = models.CharField(max_length=200)
-        self.text = models.TextField()
-        self.created_date = models.DateTimeField(
-            default=timezone.now
-        )
-        self.published_date = models.DateTimeField(
-            blank=True, null=True
-        )
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now
+    )
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
 
     def publish(self):
         self.published_date = timezone.now()
